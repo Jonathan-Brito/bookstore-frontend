fc67ceedc84a0424fb62bd062eaff4e42ecea820
Jonathan-Brito <devjonathanbrito@gmail.com>
2021-08-21 17:10:22 -0300

    Adcionando o metodo delete

--- src/app/components/views/categoria/categoria.service.ts
+++ src/app/components/views/categoria/categoria.service.ts
@@ -19,6 +19,11 @@ export class CategoriaService {
     return this.http.get<Categoria[]>(url)
   }
 
+  findById(id: String): Observable<Categoria>{
+    const url = `${this.baseUrl}/categories/${id}`
+    return this.http.get<Categoria>(url)
+  }
+
   create(categoria : Categoria): Observable<Categoria>{
     const url = `${this.baseUrl}/categories`
     return this.http.post<Categoria>(url, categoria);
